639f6ec90be2a3dc0a2e50d3c12c869424f22ced
rybczany <piotr.maciej.rybak@gmail.com>
2017-09-09 01:04:19 +0200

    mouseenter()

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
 
+	var paris = $('.location.location--paris');
+	var listParis = $('.paris');
+	var textHeader = $('.text-content__header');
+	var placeList = $('.list li');
+	console.log(placeList)
+
+	listParis.mouseenter(function(){
+		if(paris.toggle('slow')) {
+			placeList.toggleClass('active') && textHeader.toggle() && listParis.toggleClass('active');
+		}
+	});
+
+
+
 
 });
