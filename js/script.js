53e0863aaf0dd5415f71c916f31ba9b95b4775b9
rybczany <piotr.maciej.rybak@gmail.com>
2017-09-09 16:30:20 +0200

    jquery-location display

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
 $(document).ready(function(){
 
 
-	var paris = $('.location.location--paris');
+	var paris = $('#paris');
+	var moscow = $('#moscow');
+	var bologna = $('#bologna');
 	var listParis = $('.paris');
+	var listMoscow = $('.moscow');
+	var listBologna = $('.bologna');
 	var textHeader = $('.text-content__header');
-	var placeList = $('.list li');
-	console.log(placeList)
+	var placeList = $('.list li span');
 
-	listParis.mouseenter(function(){
-		if(paris.toggle('slow')) {
-			placeList.toggleClass('active') && textHeader.toggle() && listParis.toggleClass('active');
+	listParis.on('mouseenter', pickUp);
+	listMoscow.on('mouseenter', pickUp);
+	listBologna.on('mouseenter', pickUp);
+
+	function pickUp(){
+
+		var allLocation = $('.location');
+		for (var i=0; i<allLocation.length; i++){
+			allLocation[i];
 		}
-	});
+
+		var pickLoc = $(this).parents('body');
+		var pick = pickLoc.find(allLocation);
+
+		if(pick.toggle('slow')){
+			placeList.toggleClass('active') && textHeader.toggle();
+		}
+		console.log(pick);
+
+	}
+
+
+
+
+	//for (var i = 0; i < placeList.length; i++){
+
+	//placeList.mouseenter(function(){
+		//if(location.toggle('slow')) {
+			//placeList.toggleClass('active') && textHeader.toggle() && listParis.toggleClass('active');
+		//}
+	//})
+	//}
 
 
 
